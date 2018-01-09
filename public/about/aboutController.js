e02d105677171787fb6fd727268fae6676edd79c
syedbaji8 <syedbaji8@gmail.com>
2018-01-09 23:45:02 +0530

    DEVELOPMENT: Whole application files

---
+++ public/about/aboutController.js
@@ -0,0 +1,7 @@
+myApp.controller('aboutController', function($scope) {
+    $scope.about = "ABOUT";
+    $scope.parentcat = "This is parent cat 'Roshel singh'";
+});
+myApp.controller('aboutInCats', function($scope) {
+    $scope.cats = "Diya mirza";
+});
