0d68efaa2b37d3b4a4a2a2f058ee0d43558549bb
syedbaji8 <syedbaji8@gmail.com>
2018-02-07 23:35:03 +0530

    development: emptying object

--- public/home/homeController.js
+++ public/home/homeController.js
@@ -1,16 +1,16 @@
 myApp.controller('userInformationController', ['$scope', '$http', function($scope, $http, $state) {
-    this.ReportId = "123456";
-    this.FirstName = "Raja";
-    this.LastName = "Hindusthani";
-    this.Gender = "Male";
-    this.HusbandWife = "India";
-    this.Age = "28";
-    this.DateOfAdmission = "25-Aug-2017";
-    this.UserInputEmail = "rajahind@gmail.com";
-    this.Mobile = "9123456789";
-    this.Address = "HSR Layout, Bangalore";
-    this.DiagnosisDescription = "Diagnosed by Viral fever";
-    this.TreatmentDescription = "Paracetemol tablet";
+    // this.ReportId = "123456";
+    // this.FirstName = "Raja";
+    // this.LastName = "Hindusthani";
+    // this.Gender = "Male";
+    // this.HusbandWife = "India";
+    // this.Age = "28";
+    // this.DateOfAdmission = "25-Aug-2017";
+    // this.UserInputEmail = "rajahind@gmail.com";
+    // this.Mobile = "9123456789";
+    // this.Address = "HSR Layout, Bangalore";
+    // this.DiagnosisDescription = "Diagnosed by Viral fever";
+    // this.TreatmentDescription = "Paracetemol tablet";
 
     this.save = function(information) {
         $http.post('/api/userInfo/all', information).then(function() {
